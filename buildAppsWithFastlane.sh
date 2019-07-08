3498e71306ec6ec6e02f937599a923f8de85325d
Juan Canepa <juan.canepa>
2019-07-08 16:28:06 -0400

    chore: se agrego el .sh buildAppsWithFastlane

---
+++ buildAppsWithFastlane.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ROOT_DIRECTORY="/Users/jacm/projects/reactFeature168_6";
+DIR_SIMULATION_BUILD="simulationBuilds";
+CURRENT_DATE=`date "+ %Y-%M-%MT%H:%M:%S".txt`;
+CURRENT_DATE=$(echo $CURRENT_DATE);
+touch "${ROOT_DIRECTORY}/${DIR_SIMULATION_BUILD}/${CURRENT_DATE}";
